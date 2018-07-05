9d186de1766ea6090b900371ea118c21f1d243aa
thepisas <thepisas@gmail.com>
2018-07-05 14:13:03 -0400

    updating comments

--- utils/oneLiners.pl
+++ utils/oneLiners.pl
@@ -1,2 +1,2 @@
-#replaces string way with day, but not the string away
+#Negative look-behind example - replaces string way with day, but not the string away
 perl -p -i -e 's~(?<!a)way~day~g' ./*.sh
