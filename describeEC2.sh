7488f0532912bfa1254ed607a809f8e566202e9b
thepisas <thepisas@gmail.com>
2018-07-05 16:24:41 -0400

    updated derivation of output_file

--- describeEC2.sh
+++ describeEC2.sh
@@ -8,6 +8,9 @@ fi
 
 mfa_code=${1}
 output_dir=../output
+#output_file=${output_dir}/ec2.csv
+#replaces listString.sh or describeString.sh to String.csv
+output_file=${output_dir}/`echo "${BASH_SOURCE[0]}"|perl -p -i -e 's~.*(list|describe)(.*)\.sh~$2.csv~'` #${BASH_SOURCE[0]} gives the script name ; it works when the script is invoked as  "source ./script.sh" and  as ./script.sh ; $0 works only when invoked as ./script.sh
 
 #unset the temp credentials (variables) set by MFA.sh during a previous run ; if not when you call MFA.sh,   the "aws sts get-session-token" call will be invoked with  your temp credentials  that was set by a previous run of MFA.sh , as oppossed to your permanent AWS cred and thus will error out  with "An error occurred (AccessDenied) when calling the GetSessionToken operation: Cannot call GetSessionToken with session credentials"
 source ./unsetTempCred.sh
@@ -15,7 +18,6 @@ source ./unsetTempCred.sh
 #get and set temporary credentials when MFA is in use ; this by default expires in 12 hours
 source ../MFA.sh ${mfa_code}  
 
-output_file=${output_dir}/ec2.csv
 
 #aws ec2 describe-instances --output text --query 'Reservations[*].Instances[*].[InstanceId, InstanceType,State.Name, LaunchTime, Placement.AvailabilityZone, Placement.Tenancy, PrivateIpAddress, PrivateDnsName, PublicDnsName, [Tags[?Key==`Name`].Value] [0][0], [Tags[?Key==`purpose`].Value] [0][0], [Tags[?Key==`environment`].Value] [0][0], [Tags[?Key==`team`].Value] [0][0] ]' > instances.csv
 #aws ec2 describe-instances --query 'Reservations[*].Instances[*].[[Tags[?Key==`Name`].Value] [0][0],InstanceId, InstanceType,Placement.AvailabilityZone,PrivateIpAddress, PublicDnsName, [Tags[?Key==`OS`].Value] [0][0],[Tags[?Key==`Application`].Value] [0][0],[Tags[?Key==`Environment`].Value] [0][0],State.Name]' > instances.json
