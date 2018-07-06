2c1747eb122cde82555e263c64f81e8601b6bb1e
Antony <thepisas@gmail.com>
2018-07-05 23:33:32 -0400

    removing perl -i flag as the input is a string and not a file.

--- describeEC2.sh
+++ describeEC2.sh
@@ -10,7 +10,7 @@ mfa_code=${1}
 output_dir=../output
 #output_file=${output_dir}/ec2.csv
 #replaces listString.sh or describeString.sh to String.csv
-output_file=${output_dir}/`echo "${BASH_SOURCE[0]}"|perl -p -i -e 's~.*(list|describe)(.*)\.sh~$2.csv~'` #${BASH_SOURCE[0]} gives the script name ; it works when the script is invoked as  "source ./script.sh" and  as ./script.sh ; $0 works only when invoked as ./script.sh
+output_file=${output_dir}/`echo "${BASH_SOURCE[0]}"|perl -p -e 's~.*(list|describe)(.*)\.sh~$2.csv~'` #${BASH_SOURCE[0]} gives the script name ; it works when the script is invoked as  "source ./script.sh" and  as ./script.sh ; $0 works only when invoked as ./script.sh
 
 #unset the temp credentials (variables) set by MFA.sh during a previous run ; if not when you call MFA.sh,   the "aws sts get-session-token" call will be invoked with  your temp credentials  that was set by a previous run of MFA.sh , as oppossed to your permanent AWS cred and thus will error out  with "An error occurred (AccessDenied) when calling the GetSessionToken operation: Cannot call GetSessionToken with session credentials"
 source ./unsetTempCred.sh
