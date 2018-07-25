f4c8aa499d524bc8a524d3a43bb685f9133710b7
thepisas <thepisas@gmail.com>
2018-07-25 14:12:06 -0400

    new files added

---
+++ cli_commands.sh
@@ -0,0 +1,29 @@
+###EC2 - Begin##################################
+Value=RedHatLinux; cmd="aws ec2 create-tags --resources \$i --tags Key=OS,Value=$Value" 
+for i in i-073861006d7f7c1ed i-0f3ae2cae4a63df29 ; do 
+  echo ${cmd} 
+  #${cmd}
+done
+
+###EC2 - End##################################
+
+###ECS - Begin##################################
+#if using MFA , call source ../MFA.sh <token> to set the credentials needed in the commands below
+aws ecs list-clusters
+
+aws ecs list-container-instances --cluster ecs-dev #cluster-name
+
+#to get info about container ec2 instances
+aws ecs describe-container-instances --cluster ecs-dev --container-instances <arn_of_ec2_instance which you can get from the command above>
+
+aws ecs list-task-definition-families
+aws ecs list-task-definitions
+aws ecs describe-task-definition --task-definition ktdev-app #task-name, by default provides the latest version
+
+aws ecs list-services --cluster ecs-dev #cluster-name
+
+#ECR
+aws ecr get-login
+aws ecr describe-repositories
+aws ecr list-images --repository-name amzecsrfdev/bucketstorageapi
+###ECS- End##################################
---
+++ getEC2LaunchTemplate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# get launch template from existing instance
+
+if [ $# -ne 2 ];then
+  echo -e "usage: . ${BASH_SOURCE[0]} <mfa_code_from_your_device> <ec2-instance-id>"
+  return 1 #using return here , instead of exit as the appropriate invocation of this script is "source ${BASH_SOURCE[0]} or . ${BASH_SOURCE[0]}" . Doing so causes the script to be executed in the current shell (instead of in another spawned shell) ; thereby the variables(temporary credentials) set in ../MFA.sh are available to you even in your current shell even after this script has ended. So after executing this script, from the same shell prompt you can subsequently run other aws commands withouth having to set the temporary credentials again. If you don't use source  (or .) , and instead simply execute this script as ${BASH_SOURCE[0]} , then a new shell will be spawned and this script will be run in that shell and so the variables set are not available to you after the script has ended .  Getting back to using return vs exit here , since  a new shell is not spawned when you use . , if you use exit here, you will lose your terminal . Normally return is used to return from a function, but in this use case, you use it to return from the executing script .
+fi
+
+mfa_code=${1}
+instance_id=${2}
+output_dir=../output
+#output_file=${output_dir}/ec2.csv
+#replaces listString.sh or describeString.sh to String.csv
+output_file=${output_dir}/`echo "${BASH_SOURCE[0]}"|perl -p -e 's~.*(list|describe|get)(.*)\.sh~$2.json~'` #${BASH_SOURCE[0]} gives the script name ; it works when the script is invoked as  "source ./script.sh" and  as ./script.sh ; $0 works only when invoked as ./script.sh
+
+#unset the temp credentials (variables) set by MFA.sh during a previous run ; if not when you call MFA.sh,   the "aws sts get-session-token" call will be invoked with  your temp credentials  that was set by a previous run of MFA.sh , as oppossed to your permanent AWS cred and thus will error out  with "An error occurred (AccessDenied) when calling the GetSessionToken operation: Cannot call GetSessionToken with session credentials"
+source ./unsetTempCred.sh
+
+#get and set temporary credentials when MFA is in use ; this by default expires in 12 hours
+source ../MFA.sh ${mfa_code}  
+
+
+#aws ec2 describe-instances --output text --query 'Reservations[*].Instances[*].[InstanceId, InstanceType,State.Name, LaunchTime, Placement.AvailabilityZone, Placement.Tenancy, PrivateIpAddress, PrivateDnsName, PublicDnsName, [Tags[?Key==`Name`].Value] [0][0], [Tags[?Key==`purpose`].Value] [0][0], [Tags[?Key==`environment`].Value] [0][0], [Tags[?Key==`team`].Value] [0][0] ]' > instances.csv
+#aws ec2 describe-instances --query 'Reservations[*].Instances[*].[[Tags[?Key==`Name`].Value] [0][0],InstanceId, InstanceType,Placement.AvailabilityZone,PrivateIpAddress, PublicDnsName, [Tags[?Key==`OS`].Value] [0][0],[Tags[?Key==`Application`].Value] [0][0],[Tags[?Key==`Environment`].Value] [0][0],State.Name]' > instances.json
+aws ec2 get-launch-template-data --instance-id ${instance_id} --query "LaunchTemplateData" > ${output_file}
+
+echo "The output is in ${output_file}"
