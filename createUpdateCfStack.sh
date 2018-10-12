e54064ef88b3db64eb5060b050d095d5a9321677
thepisas <thepisas@gmail.com>
2018-10-12 17:32:07 -0400

    new files

---
+++ createUpdateCfStack.sh
@@ -0,0 +1,22 @@
+if [ $# -ne 5 ];then
+  echo -e "usage: ${BASH_SOURCE[0]} stack_name cfn_template_file Application operation"
+  echo -e "e.g ${BASH_SOURCE[0]} RStudioDev ec2.cfn.json create"
+  exit 1
+fi
+stack_name=${1}
+cfn_template_file=${2}
+Application=${3}
+operation=${4}
+parameter_dir="."
+parameter_file="${stack_name}.par"
+if [ ${operation} = 'create' ]; then
+	cmd="aws cloudformation create-stack --stack-name ${stack_name}  --template-body file://./${cfn_template_file} --parameters file://${parameter_dir}/${parameter_file} --capabilities CAPABILITY_NAMED_IAM --timeout-in-minutes 8 --tags Key=Application,Value=${Application}"
+elif [ ${operation} = 'update' ]; then
+	cmd="aws cloudformation update-stack --stack-name ${stack_name}  --template-body file://./${cfn_template_file} --parameters file://${parameter_dir}/${parameter_file} --capabilities CAPABILITY_NAMED_IAM"
+else 
+	echo "Invalid value entered for operation ... exiting" ; exit 1
+fi
+echo "${cmd}"
+#echo -n "If you want to execute the above command hit ENTER"
+#read input
+eval $cmd
