5d21bdc9ff44eb90384e88e551f1449c6cb09831
thepisas <thepisas@gmail.com>
2018-10-17 17:59:06 -0400

    updates

--- createUpdateCfStack.sh
+++ createUpdateCfStack.sh
@@ -1,14 +1,14 @@
-if [ $# -ne 4 ];then
-  echo -e "usage: ${BASH_SOURCE[0]} stack_name cfn_template_file Application operation"
-  echo -e "e.g ${BASH_SOURCE[0]} RStudioDev ec2.cfn.json RStudio create"
+if [ $# -ne 5 ];then
+  echo -e "usage: ${BASH_SOURCE[0]} stack_name cfn_template_file parameter_file Application operation"
+  echo -e "e.g ${BASH_SOURCE[0]} RStudioDev ec2.cfn.json RStudio.par RStudio create"
   exit 1
 fi
 stack_name=${1}
 cfn_template_file=${2}
-Application=${3}
-operation=${4}
 parameter_dir="."
-parameter_file="${stack_name}.par"
+parameter_file="${3}"
+Application=${4}
+operation=${5}
 if [ ${operation} = 'create' ]; then
 	cmd="aws cloudformation create-stack --stack-name ${stack_name}  --template-body file://./${cfn_template_file} --parameters file://${parameter_dir}/${parameter_file} --capabilities CAPABILITY_NAMED_IAM --timeout-in-minutes 8 --tags Key=Application,Value=${Application}"
 elif [ ${operation} = 'update' ]; then
